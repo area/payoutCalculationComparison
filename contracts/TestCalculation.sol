7b7d39600e1acfa4a3ebdbeefac4e4499c81c111
Filip Lazovic <lazoviccorp@gmail.com>
2018-04-26 22:11:19 +0200

    Updated implementation1 to support wider range of inputs

--- contracts/TestCalculation.sol
+++ contracts/TestCalculation.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.4.3;
 contract TestCalculation {
  function implementation1(uint256 availableAmount, uint256 r, uint256 t, uint256 R, uint256 T) public pure returns (uint256) {
    uint256 numerator = sqrt(r) * sqrt(t);
-   uint256 denumerator = sqrt(R) * sqrt(T);
-   uint256 d = numDigits(denumerator) / 2;
-   uint256 factor = 10 ** d;
-   uint256 res = numerator * factor / (denumerator / factor);
+   uint256 denomerator = sqrt(R) * sqrt(T);
+   uint256 numeratorLength = numDigits(numerator);
+   uint256 denomeratorLength = numDigits(denomerator) / 2;
+   uint256 factor = 10 ** ((numeratorLength + denomeratorLength) > 78 ? (78 - numeratorLength - 1) : denomeratorLength);
+   uint256 res = numerator * factor / (denomerator / factor);
    return mul(res, (availableAmount / factor)) / factor;
-   /* return numerator; */
   }
 
   /* function implementation1(uint256 availableAmount, uint256 r, uint256 t, uint256 R, uint256 T) public pure returns (uint256) {
--- test/metacoin.js
+++ test/metacoin.js
@@ -13,22 +13,26 @@ function len(n) {
 
 function BigNumberCalculation(r){
   const numerator = r.userTokens.times(r.userReputation);
+  console.log(numerator.toString());
   const denominator = r.totalReputation.times(r.totalTokens);
+  console.log(denominator.toString());
   const a = numerator.div(denominator).sqrt();
+  console.log(a.toString());
+  console.log(r.payoutAmount.times(a).toString());
   return r.payoutAmount.times(a);
 }
 
 function BigNumberCalculationINT(r){
-  const numerator = r.userTokens.times(r.userReputation).sqrt();
-  console.log(len(r.userTokens.times(r.userReputation)), 't*r');
-  const denominator = r.totalReputation.times(r.totalTokens).sqrt();
-  console.log(len(r.totalReputation.times(r.totalTokens)), 'T*R');
-  const diff = BigNumber(77).minus(len(numerator)).toNumber();
-  const factor = BigNumber(10).pow((diff % 2 === 0) ? diff : diff - 1);
-  console.log(len(factor), 'factor');
+  const numerator = r.userTokens.sqrt().times(r.userReputation.sqrt());
+  console.log(numerator.toString(), 't*r');
+  const denominator = r.totalReputation.sqrt().times(r.totalTokens.sqrt());
+  console.log(denominator.toString(), 'T*R');
+  const factor = BigNumber(10).pow(Math.floor(len(denominator)/2));
+  console.log(factor.toString(), 'factor');
   const a = numerator.times(factor).div(denominator);
-  console.log(len(numerator.times(factor)), 't*r*factor');
-  console.log(len(r.payoutAmount.times(a)), 'res');
+  console.log(numerator.times(factor).toString(), 'numerator*factor');
+  console.log(a.toString(), 'res');
+  console.log(r.payoutAmount.times(a).div(factor).toString(), 'data');
   return r.payoutAmount.times(a).div(factor);
 }
 
@@ -69,26 +73,41 @@ contract('TestCalculation', function(accounts) {
         userTokens:BigNumber("1000000000000000000000000000000000000"),
         payoutAmount:BigNumber("2000000000000000000000000000000000000000000000000000000000000"),
       },
+      {
+        totalReputation: BigNumber("27836472836478263784623876428736487236428"),
+        userReputation: BigNumber("2397429834782374223974298347823742"),
+        totalTokens:BigNumber("2837429374236748263784623876478236487236748623874623876234682"),
+        userTokens:BigNumber("4523145231452314523145231452314523112"),
+        payoutAmount:BigNumber("8275893475873498573984759834759834957349875938475983749857348"),
+      },
+      {
+        totalReputation: BigNumber("27836472836478263784623876428736487236428278364728364782637846238764287364872"),
+        userReputation: BigNumber("2397429834782374223974298347823742239742983478237422397429834782374"),
+        totalTokens:BigNumber("28374293742367482637846238764782364872367486238746238762346822837429374236748"),
+        userTokens:BigNumber("4523145231452314523145231452314523112452314523145231452314523145231452311"),
+        payoutAmount:BigNumber("82758934758734985739847598347598349573498759384759837498573488275893475873498"),
+      },
     ];
   reputations.forEach(data => {
     it("implementation1", async () => {
       m = await TestCalculation.deployed();
-      // const gas = await m.implementation1.estimateGas(data.payoutAmount.toString(), data.userReputation.toString(), data.userTokens.toString(), data.totalReputation.toString(), data.totalTokens.toString());
+      const gas = await m.implementation1.estimateGas(data.payoutAmount.toString(), data.userReputation.toString(), data.userTokens.toString(), data.totalReputation.toString(), data.totalTokens.toString());
       const res = await m.implementation1(data.payoutAmount.toString(), data.userReputation.toString(), data.userTokens.toString(), data.totalReputation.toString(), data.totalTokens.toString());
       const onChainResult = BigNumber(res.toString());
-      // const preciseResult = BigNumberCalculation(data);
+      BigNumberCalculation(data);
+      const preciseResult = BigNumberCalculationINT(data);
       console.log("");
       console.log("On chain result: ", onChainResult.toString());
-      // console.log("Precise result", preciseResult.toString());
-      // console.log("");
-      // console.log("User Tokens: ", data.userTokens.toString());
-      // console.log("User Reputation: ", data.userReputation.toString());
-      // console.log("Total Tokens: ", data.totalTokens.toString());
-      // console.log("Total Reputation: ", data.totalReputation.toString());
-      // console.log("Reward pool: ", data.payoutAmount.toString());
-      // console.log("Percentage Wrong: ", onChainResult.minus(preciseResult).div(preciseResult).times(100).toString(), "%");
-      // console.log("Absolute Wrong: ", onChainResult.minus(preciseResult).toString())
-      // console.log("Estimated Gas cost: ", gas.toString());
+      console.log("Precise result", preciseResult.toString());
+      console.log("");
+      console.log("User Tokens: ", data.userTokens.toString());
+      console.log("User Reputation: ", data.userReputation.toString());
+      console.log("Total Tokens: ", data.totalTokens.toString());
+      console.log("Total Reputation: ", data.totalReputation.toString());
+      console.log("Reward pool: ", data.payoutAmount.toString());
+      console.log("Percentage Wrong: ", onChainResult.minus(preciseResult).div(preciseResult).times(100).toString(), "%");
+      console.log("Absolute Wrong: ", onChainResult.minus(preciseResult).toString())
+      console.log("Estimated Gas cost: ", gas.toString());
       console.log("");
     });
   });
